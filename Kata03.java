8f583a68bb2d1cc41c74e5216c271569f31e2927
Chris Stone <christopher-ms@hotmail.com>
2015-01-02 23:04:16 +0000

    Made start to Kata03

---
+++ Kata03.java
@@ -0,0 +1,15 @@
+class Kata03 {
+
+        public static void main(String[] args) {
+                commonSeg(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        }
+
+        public static String commonSeg(int num1, int num2) {
+
+
+
+
+                return null;
+        }
+
+}
